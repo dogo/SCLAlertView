2c51f626a048cc46bbe6fc1ea669e2ee934be998
Diogo Autilio <diogo.autilio@titansgroup.com.br>
2015-03-23 10:14:44 -0300

    Fix shouldDismissOnTapOutside not working #87

--- SCLAlertView/SCLAlertView.m
+++ SCLAlertView/SCLAlertView.m
@@ -225,21 +225,23 @@ NSTimer *durationTimer;
     self.backgroundView.frame = newBackgroundFrame;
     
     // Set new main frame
-    CGRect newbaseFrame = self.view.frame;
-    newbaseFrame.size = sz;
-    self.view.frame = newbaseFrame;
+    CGRect r;
+    if (self.view.superview != nil)
+    {
+        // View is showing, position at center of screen
+        r = CGRectMake((sz.width-_windowWidth)/2, (sz.height-_windowHeight)/2, _windowWidth, _windowHeight);
+    }
+    else
+    {
+        // View is not visible, position outside screen bounds
+        r = CGRectMake((sz.width-_windowWidth)/2, -_windowHeight, _windowWidth, _windowHeight);
+    }
     
     // Set frames
-    {
-        CGFloat x = (sz.width - _windowWidth) / 2;
-        CGFloat y = (sz.height - _windowHeight -  (kCircleHeight / 8)) / 2;
-        
-        _contentView.frame = CGRectMake(x, y, _windowWidth, _windowHeight);
-        y -= kCircleHeightBackground * 0.6f;
-        x = (sz.width - kCircleHeightBackground) / 2;
-        _circleViewBackground.frame = CGRectMake(x, y, kCircleHeightBackground, kCircleHeightBackground);
-        _circleIconImageView.frame = CGRectMake(kCircleHeight / 2 - _circleIconHeight / 2, kCircleHeight / 2 - _circleIconHeight / 2, _circleIconHeight, _circleIconHeight);        
-    }
+    self.view.frame = r;
+    _contentView.frame = CGRectMake(0.0f, kCircleHeight / 4, _windowWidth, _windowHeight);
+    _circleViewBackground.frame = CGRectMake(_windowWidth / 2 - kCircleHeightBackground / 2, kCircleBackgroundTopPosition, kCircleHeightBackground, kCircleHeightBackground);
+    _circleIconImageView.frame = CGRectMake(kCircleHeight / 2 - _circleIconHeight / 2, kCircleHeight / 2 - _circleIconHeight / 2, _circleIconHeight, _circleIconHeight);
     
     // Text fields
     {
