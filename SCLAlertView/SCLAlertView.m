9b4e29f8e753ba16e611c8317dcc84bf14bb8cd0
André Felipe Santos <a.felipe@ymail.com>
2016-02-01 09:00:45 -0200

    Revert "Fix wrong label position"
    This reverts commit 1d6f1e649860dd975b39bed7f7302d868a32f578.

--- SCLAlertView/SCLAlertView.m
+++ SCLAlertView/SCLAlertView.m
@@ -231,8 +231,11 @@ SCLTimerDisplay *buttonTimer;
     _contentView.layer.cornerRadius = 5.0f;
     _contentView.layer.masksToBounds = YES;
     _contentView.layer.borderWidth = 0.5f;
-    
-    [_contentView addSubview:_labelTitle];
+
+    CGRect position = [self.contentView convertRect:self.labelTitle.frame toView:self.view];
+    _labelTitle.frame = position;
+    [self.view addSubview:_labelTitle];
+
     [_contentView addSubview:_viewText];
     
     // Colors
