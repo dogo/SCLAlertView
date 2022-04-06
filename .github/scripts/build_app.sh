09245fdcc5d699269fd18af4bd0eb4d471291c66
Diogo Autilio <diautilio@gmail.com>
2022-04-05 21:02:53 -0300

    Update build_app.sh

--- .github/scripts/build_app.sh
+++ .github/scripts/build_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -project SCLAlertView.xcodeproj \
             -scheme SCLAlertView \
-            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean build | xcpretty
