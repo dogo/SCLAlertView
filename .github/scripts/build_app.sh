0540be42283cf96f5af52ffc624ac1f09ce1cb26
Diogo Autilio <diautilio@gmail.com>
2025-11-30 08:32:58 -0300

    chore(ci): update building system

--- .github/scripts/build_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -project SCLAlertView.xcodeproj \
-            -scheme SCLAlertView \
-            -destination platform=iOS\ Simulator,OS=26.1,name=iPhone\ 16 \
-            clean build | xcpretty
